52244941961ee5e22ade43eff2eb2d677cb2df0a
shankar327 <shanalashankar036@gmail.com>
2026-04-18 22:43:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("🏦 Sumalatha Loan Assistance")
+
+st.write("Apply for loans easily")
+
+name = st.text_input("Enter your name")
+phone = st.text_input("Enter phone number")
+
+loan = st.selectbox("Select Loan Type",
+["Home Loan", "Personal Loan", "Education Loan", "Gold Loan"])
+
+if st.button("Submit"):
+    st.success("Submitted Successfully!")
